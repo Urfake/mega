9ede152c8ca3202bfce916211abf8b74d47b5b43
Urfake <sharshenkulov.1@gmail.com>
2024-10-04 18:06:33 +0600

    Creating the project

---
+++ src/main/java/com/example/mega_task/MegaTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.mega_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mega_task/MegaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mega_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
