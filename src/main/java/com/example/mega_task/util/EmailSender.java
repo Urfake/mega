d8c8f54be3112a4272f8cc0365385202e612620e
Urfake <sharshenkulov.1@gmail.com>
2024-10-07 05:22:21 +0600

    Email sender

---
+++ src/main/java/com/example/mega_task/util/EmailSender.java
@@ -0,0 +1,24 @@
+package com.example.mega_task.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+public class EmailSender {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendEmail(String toEmail, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        //Нужно будет поменять(также в application.properties прописать username и password
+        message.setFrom("sharshenkulov.1@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        javaMailSender.send(message);
+    }
+}
--- src/test/java/com/example/mega_task/service/impl/BookServiceImplTest.java
+++ src/test/java/com/example/mega_task/service/impl/BookServiceImplTest.java
@@ -1,2 +1,56 @@
-package com.example.mega_task.service.impl;public class BookServiceImplTest {
+package com.example.mega_task.service.impl;
+
+import com.example.mega_task.entities.Book;
+import com.example.mega_task.repos.BookRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+class BookServiceImplTest {
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @InjectMocks
+    private BookServiceImpl bookService;
+
+    @Test
+    void shouldReturnBooksFilteredByGenre() {
+        Long id = 1L;
+        List<Book> books = createBooks();
+
+        Mockito.when(bookRepository.findById(id)).thenReturn(Optional.ofNullable(books.get(id.intValue())));
+
+        Optional<Book> result = bookService.getById(id);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(Optional.ofNullable(books.get(id.intValue())), result);
+    }
+
+    private List<Book> createBooks() {
+        Book firstBook = new Book();
+        Book secondBook = new Book();
+
+        firstBook.setId(1L);
+        firstBook.setTitle("11 of September");
+        firstBook.setAvailable(true);
+
+        secondBook.setId(2L);
+        secondBook.setTitle("Data science");
+        secondBook.setAvailable(true);
+
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(firstBook);
+        bookList.add(secondBook);
+        return bookList;
+    }
 }
